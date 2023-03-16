27d70614183eae58bde51649684d3e490ada79ec
Faruk Avcı <55799734+Faruk372742@users.noreply.github.com>
2023-03-16 23:58:40 +0300

    TR70

--- consts/paths.ts
+++ consts/paths.ts
@@ -2,11 +2,12 @@ export enum PATHS {
   "LOGIN" = "LOGIN",
   "REGISTER" = "REGISTER",
   "ACCOUNT" = "ACCOUNT",
-  "HOME" = "HOME",
+  "HOME" = "Home",
   "LEADERBOARD" = "LEADERBOARD",
   "SETTINGS" = "SETTINGS",
   "PLAY" = "PLAY",
   "EDIT_PROFILE" = "EditProfile",
   "JOIN" = "JOIN",
   "MAPS" = "MAPS",
+  "FINISHED_TREASURES" = "FINISHED_TREASURES",
 }
---
+++ pages/FinishedTreasuresOnMap.tsx
@@ -0,0 +1,85 @@
+import { SafeAreaView, StyleSheet, View, Image } from "react-native";
+import { useTheme } from "../theme";
+import { Theme } from "../theme/types";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import { Dimensions } from "react-native";
+
+export function FinishedMapsOnMap() {
+  const { theme } = useTheme();
+  const themedStyles = styles(theme);
+  const { height, width } = Dimensions.get("window");
+  const LATITUDE_DELTA = 17;
+  const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
+
+  const mockFinishedTreasures = [
+    {
+      latitude: 41.441,
+      longitude: 31.80889,
+    },
+    {
+      latitude: 37.93976,
+      longitude: 32.44143,
+    },
+    {
+      latitude: 38.48113,
+      longitude: 27.14095,
+    },
+    {
+      latitude: 38.48113,
+      longitude: 27.14095,
+    },
+    {
+      latitude: 37.65929,
+      longitude: 36.8606,
+    },
+  ];
+  return (
+    <SafeAreaView style={themedStyles.container}>
+      <MapView
+        style={themedStyles.map}
+        initialRegion={{
+          latitude: 39.19574,
+          longitude: 33.41181,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+        scrollEnabled={true}
+        zoomControlEnabled={true}
+      >
+        {mockFinishedTreasures.map((e, index) => (
+          <View key={index}>
+            <Marker coordinate={e}>
+              <Image
+                source={require("../assets/images/treasure.png")}
+                style={themedStyles.markerStyle}
+              />
+            </Marker>
+          </View>
+        ))}
+      </MapView>
+    </SafeAreaView>
+  );
+}
+
+const styles = (theme: Theme) => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      width: "100%",
+      backgroundColor: theme.appBackground.backgroundColor,
+    },
+    scrollViewStyle: {
+      width: "100%",
+      flex: 1,
+    },
+    map: {
+      width: "100%",
+      height: "100%",
+    },
+    markerStyle: {
+      width: 32,
+      height: 32,
+      resizeMode: "contain",
+    },
+  });
+};
