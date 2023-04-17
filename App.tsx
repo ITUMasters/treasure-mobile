505246637839dcaab44f7f621c0858f1e454a8b1
KartalAlp <alpkartal61@gmail.com>
2023-04-17 20:44:47 +0300

    feat: Implemented FlatList

--- App.tsx
+++ App.tsx
@@ -1,37 +1,38 @@
-import "react-native-gesture-handler";
-import * as SplashScreen from "expo-splash-screen";
-import { useAppFonts } from "./hooks";
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { ThemeContextProvider, useTheme } from "./theme";
-import { Theme } from "./theme/types";
-import { LoginPage } from "./pages/LoginPage";
-import { RegisterPage } from "./pages/RegisterPage";
-import { NavBar } from "./ui/NavBar";
-import { InGamePage } from "./pages/InGamePage";
-import { OTP_VerificationPage } from "./pages/OTP_VerificationPage";
-import { ProfilePage } from "./pages/ProfilePage";
+import 'react-native-gesture-handler';
+import * as SplashScreen from 'expo-splash-screen';
+import { useAppFonts } from './hooks';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ThemeContextProvider, useTheme } from './theme';
+import { Theme } from './theme/types';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { NavBar } from './ui/NavBar';
+import { InGamePage } from './pages/InGamePage';
+import { OTP_VerificationPage } from './pages/OTP_VerificationPage';
+import { ProfilePage } from './pages/ProfilePage';
 import {
   createDrawerNavigator,
   DrawerNavigationOptions,
-} from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
-import { getHeaderStylesByTheme } from "./utils/HeaderStyles";
+} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { getHeaderStylesByTheme } from './utils/HeaderStyles';
 
-import { RecoilRoot } from "recoil";
-import { JoinPage } from "./pages/JoinPage";
+import { RecoilRoot } from 'recoil';
+import { JoinPage } from './pages/JoinPage';
 
-import { EditProfilePage } from "./pages/EditProfilePage";
+import { EditProfilePage } from './pages/EditProfilePage';
 
-import { HomePage } from "./pages/HomePage";
-import { color } from "react-native-reanimated";
-import { colors } from "./theme/colors";
-import { SettingsPage } from "./pages/SettingsPage";
-import { MapPageTries } from "./pages/MapPageTries";
-import { FinishedMapsOnMap } from "./pages/FinishedTreasuresOnMap";
-import { useAuth } from "./recoil-store/auth/AuthStoreHooks";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { authorizedQueryClient, unauthorizedQueryClient } from "./react-query";
+import { HomePage } from './pages/HomePage';
+import { color } from 'react-native-reanimated';
+import { colors } from './theme/colors';
+import { SettingsPage } from './pages/SettingsPage';
+import { MapPageTries } from './pages/MapPageTries';
+import { FinishedMapsOnMap } from './pages/FinishedTreasuresOnMap';
+import { useAuth } from './recoil-store/auth/AuthStoreHooks';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { authorizedQueryClient, unauthorizedQueryClient } from './react-query';
+import { PaginationContextProvider } from './context/PaginationContext';
 
 export default function App() {
   const [fontsLoaded] = useAppFonts();
@@ -59,9 +60,11 @@ function AppWithRecoil() {
 
   return (
     <NavigationContainer>
-      <ThemeContextProvider>
-        {auth ? <AuthorizedApp /> : <UnauthorizedApp />}
-      </ThemeContextProvider>
+      <PaginationContextProvider>
+        <ThemeContextProvider>
+          {auth ? <AuthorizedApp /> : <UnauthorizedApp />}
+        </ThemeContextProvider>
+      </PaginationContextProvider>
     </NavigationContainer>
   );
 }
@@ -75,7 +78,7 @@ function AuthorizedApp() {
     <QueryClientProvider client={authorizedQueryClient}>
       <Drawer1.Navigator
         screenOptions={{
-          drawerType: "front",
+          drawerType: 'front',
           headerTintColor: colors.white,
           drawerStyle: usedStyles.drawerStyle,
           drawerLabelStyle: { color: colors.white },
@@ -85,12 +88,12 @@ function AuthorizedApp() {
         <Drawer1.Screen
           name="Profile"
           component={ProfilePage}
-          options={{ ...navbarHeaderOptions, title: "Profile Page" }}
+          options={{ ...navbarHeaderOptions, title: 'Profile Page' }}
         />
         <Drawer1.Screen
           name="Home"
           component={HomePage}
-          options={{ ...navbarHeaderOptions, title: "Home Page" }}
+          options={{ ...navbarHeaderOptions, title: 'Home Page' }}
         />
         {
           <Drawer1.Screen
@@ -98,7 +101,7 @@ function AuthorizedApp() {
             component={InGamePage}
             options={{
               ...navbarHeaderOptions,
-              title: "In Game Page",
+              title: 'In Game Page',
               drawerItemStyle: { height: 0 },
             }}
           />
@@ -108,14 +111,14 @@ function AuthorizedApp() {
           component={EditProfilePage}
           options={{
             ...navbarHeaderOptions,
-            title: "Edit Profile Page",
+            title: 'Edit Profile Page',
             drawerItemStyle: { height: 0 },
           }}
         />
         <Drawer1.Screen
           name="Join"
           component={JoinPage}
-          options={{ ...navbarHeaderOptions, title: "Join Page" }}
+          options={{ ...navbarHeaderOptions, title: 'Join Page' }}
         />
         {/*<Drawer1.Screen
           name="SETTINGS"
@@ -127,7 +130,7 @@ function AuthorizedApp() {
           component={MapPageTries}
           options={{
             ...navbarHeaderOptions,
-            title: "Map Page",
+            title: 'Map Page',
             drawerItemStyle: { height: 0 },
           }}
         />
@@ -136,7 +139,7 @@ function AuthorizedApp() {
           component={FinishedMapsOnMap}
           options={{
             ...navbarHeaderOptions,
-            title: "Completed Treasures",
+            title: 'Completed Treasures',
             drawerItemStyle: { height: 0 },
           }}
         />
@@ -154,7 +157,7 @@ function UnauthorizedApp() {
     <QueryClientProvider client={unauthorizedQueryClient}>
       <Drawer.Navigator
         screenOptions={{
-          drawerType: "front",
+          drawerType: 'front',
           headerTintColor: colors.white,
           drawerStyle: usedStyles.drawerStyle,
           drawerLabelStyle: { color: colors.white },
@@ -164,12 +167,12 @@ function UnauthorizedApp() {
         <Drawer.Screen
           name="Login"
           component={LoginPage}
-          options={{ ...navbarHeaderOptions, title: "Login Page" }}
+          options={{ ...navbarHeaderOptions, title: 'Login Page' }}
         />
         <Drawer.Screen
           name="Register"
           component={RegisterPage}
-          options={{ ...navbarHeaderOptions, title: "Register Page" }}
+          options={{ ...navbarHeaderOptions, title: 'Register Page' }}
         />
       </Drawer.Navigator>
     </QueryClientProvider>
@@ -181,11 +184,12 @@ const styles = (theme: Theme) => {
     container: {
       flex: 1,
       backgroundColor: theme.appBackground.backgroundColor,
-      alignItems: "center",
-      justifyContent: "center",
+      alignItems: 'center',
+      justifyContent: 'center',
     },
     drawerStyle: {
       backgroundColor: colors.lightRoyalBlue,
     },
   });
 };
+
