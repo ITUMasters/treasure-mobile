31b2154d158d60a3fe0755091f53e76e7661cdc5
Faruk Avcı <55799734+Faruk372742@users.noreply.github.com>
2023-05-04 12:46:31 +0300

    fix/AccountEditBug

--- react-query/queries.ts
+++ react-query/queries.ts
@@ -1,82 +1,81 @@
-import { axios } from '../react-query';
-import { TreasureSubmission, User } from './types';
+import { axios } from "../react-query";
+import { TreasureSubmission, User } from "./types";
 
 export const apiRegister = (data: User) => {
-  return axios({ url: '/user/register', method: 'post', data });
+  return axios({ url: "/user/register", method: "post", data });
 };
 
 export const apiLogin = (data: { email: string; password: string }) => {
-  return axios({ url: '/user/login', method: 'post', data });
+  return axios({ url: "/user/login", method: "post", data });
 };
 
 export const apiGetUser = (userId: number) => {
-  return axios({ url: '/user/' + userId.toString(), method: 'get' });
+  return axios({ url: "/user/" + userId.toString(), method: "get" });
 };
 
 export const apiChangeAccountInfo = (data: {
   name: string;
   surname: string;
   username: string;
-  id: number;
+  id?: number;
 }) => {
-  return axios({ url: '/user/', method: 'put', data });
+  return axios({ url: "/user/", method: "put", data });
 };
 
 export const apiGetAllTreasures = () => {
-  return axios({ url: '/treasure/', method: 'get' });
+  return axios({ url: "/treasure/", method: "get" });
 };
 
 export const apiGetTreasureById = (treasureId: number) => {
-  return axios({ url: '/treasure/' + treasureId.toString(), method: 'get' });
+  return axios({ url: "/treasure/" + treasureId.toString(), method: "get" });
 };
 
 export const apiGetHintsByTreasureId = (treasureId: number) => {
   return axios({
-    url: '/hint/treasure/' + treasureId.toString(),
-    method: 'get',
+    url: "/hint/treasure/" + treasureId.toString(),
+    method: "get",
   });
 };
 
 export const apiGetTreasureByPageId = (
   pageId: number,
-  regionId: number | null,
+  regionId: number | null
 ) => {
   return axios({
-    url: '/treasure/page/' + pageId.toString(),
-    method: 'get',
+    url: "/treasure/page/" + pageId.toString(),
+    method: "get",
     params: { regionId: regionId },
   });
 };
 
 export const apiPurchaseHint = (data: { hintId: number }) => {
   return axios({
-    url: '/hint/' + data.hintId.toString() + '/purchase',
-    method: 'post',
+    url: "/hint/" + data.hintId.toString() + "/purchase",
+    method: "post",
   });
 };
 
 export const apiJoin = (data: { treasureId: number }) => {
   return axios({
-    url: '/treasure/' + data.treasureId.toString() + '/join',
-    method: 'post',
+    url: "/treasure/" + data.treasureId.toString() + "/join",
+    method: "post",
   });
 };
 
 export const apiTreasureSubmission = (data: TreasureSubmission) => {
   return axios({
-    url: '/treasureSubmission',
-    method: 'post',
+    url: "/treasureSubmission",
+    method: "post",
     data,
   });
 };
 
 export const apiGetTreasureSubmissionByInteractionId = (
-  interactionId: number,
+  interactionId: number
 ) => {
   return axios({
-    url: '/treasureSubmission',
-    method: 'get',
+    url: "/treasureSubmission",
+    method: "get",
     params: { interactionId: interactionId },
   });
 };
-
